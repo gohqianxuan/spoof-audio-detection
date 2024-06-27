9165a997a21a2cb4005aaaf931c8bce9c0b9b821
GOH QIAN XUAN <94182074+gohqianxuan@users.noreply.github.com>
2024-06-27 22:02:20 +0800

    Update Home.py
    Updated logo position in sidebar

--- Home.py
+++ Home.py
@@ -126,9 +126,9 @@ sidebarLogo = """
         [data-testid="stSidebarHeader"] {
             background-image: url(https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmRscmdoZXdvY3VsbWg2ZzA2NzE2d3VhdHdtejJ6b2VkeTA2NmRkaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ASBM73xrwXA7ij894w/giphy.gif);
             background-repeat: no-repeat;
-            background-position: 20px 20px;
+            background-position: 20px 12px;
             background-size: 280px;
-            height: 48%;
+            height: 25%;
         }
     </style>
     """
