278e79c9a26d39793de082e85a15257b8bc79d02
GOH QIAN XUAN <94182074+gohqianxuan@users.noreply.github.com>
2024-06-27 21:53:56 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -123,10 +123,9 @@ background = """
 
 sidebarLogo = """
     <style>    
-        [data-testid="stSidebarNav"] {
+        [data-testid="stSidebarHeader"] {
             background-image: url(https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmRscmdoZXdvY3VsbWg2ZzA2NzE2d3VhdHdtejJ6b2VkeTA2NmRkaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ASBM73xrwXA7ij894w/giphy.gif);
             background-repeat: no-repeat;
-            padding-top: 85px;
             background-position: 20px 20px;
             background-size: 280px;
             height: 48%;
